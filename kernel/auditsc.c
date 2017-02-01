b53504056e3c2713b67a2bdd7c957cce6b66a52a
Jan Kara <jack@suse.cz>
2017-02-01 09:21:58 +0100

    fsnotify: Free fsnotify_mark_connector when there is no mark attached 08991e83b7286635167bab40927665a90fb00d81 sketch merge

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -1586,8 +1586,7 @@ static inline void handle_one(const struct inode *inode)
 	struct audit_tree_refs *p;
 	struct audit_chunk *chunk;
 	int count;
-	if (likely(!inode->i_fsnotify_marks ||
-		   hlist_empty(&inode->i_fsnotify_marks->list)))
+	if (likely(!inode->i_fsnotify_marks))
 		return;
 	context = current->audit_context;
 	p = context->trees;
@@ -1630,8 +1629,7 @@ retry:
 	seq = read_seqbegin(&rename_lock);
 	for(;;) {
 		struct inode *inode = d_backing_inode(d);
-		if (inode && unlikely(inode->i_fsnotify_marks &&
-		    !hlist_empty(&inode->i_fsnotify_marks->list))) {
+		if (inode && unlikely(inode->i_fsnotify_marks)) {
 			struct audit_chunk *chunk;
 			chunk = audit_tree_lookup(inode);
 			if (chunk) {
