2e94655de09d088f18d17d2e4ffd9b00fa05e45b
root <root@dstbx.speedport.ip>
2026-01-31 13:47:00 +0100

    I used wrong patch..

--- lib/idr.c
+++ lib/idr.c
@@ -30,7 +30,6 @@
 #include <linux/idr.h>
 #include <linux/spinlock.h>
 #include <linux/percpu.h>
-#include <linux/xarray.h>
 
 #define MAX_IDR_SHIFT		(sizeof(int) * 8 - 1)
 #define MAX_IDR_BIT		(1U << MAX_IDR_SHIFT)
@@ -44,6 +43,7 @@
 static struct kmem_cache *idr_layer_cache;
 static DEFINE_PER_CPU(struct idr_layer *, idr_preload_head);
 static DEFINE_PER_CPU(int, idr_preload_cnt);
+static DEFINE_SPINLOCK(simple_ida_lock);
 
 /* the maximum ID which can be allocated given idr->layers */
 static int idr_max(int layers)
@@ -1075,21 +1075,29 @@ EXPORT_SYMBOL(ida_destroy);
  *
  * Use ida_simple_remove() to get rid of an id.
  */
-int ida_alloc_range(struct ida *ida, unsigned int min, unsigned int max,
-		   gfp_t gfp)
+int ida_simple_get(struct ida *ida, unsigned int start, unsigned int end,
+		   gfp_t gfp_mask)
 {
 	int ret, id;
+	unsigned int max;
 	unsigned long flags;
 
-	if ((int)min < 0)
-		return -ENOSPC;
+	BUG_ON((int)start < 0);
+	BUG_ON((int)end < 0);
 
-	if ((int)max < 0)
-		max = INT_MAX;
+	if (end == 0)
+		max = 0x80000000;
+	else {
+		BUG_ON(end < start);
+		max = end - 1;
+	}
 
 again:
-	xa_lock_irqsave(&ida->ida_rt, flags);
-	ret = ida_get_new_above(ida, min, &id);
+	if (!ida_pre_get(ida, gfp_mask))
+		return -ENOMEM;
+
+	spin_lock_irqsave(&simple_ida_lock, flags);
+	ret = ida_get_new_above(ida, start, &id);
 	if (!ret) {
 		if (id > max) {
 			ida_remove(ida, id);
@@ -1098,33 +1106,30 @@ again:
 			ret = id;
 		}
 	}
-	xa_unlock_irqrestore(&ida->ida_rt, flags);
+	spin_unlock_irqrestore(&simple_ida_lock, flags);
 
-	if (unlikely(ret == -EAGAIN)) {
-		if (!ida_pre_get(ida, gfp))
-			return -ENOMEM;
+	if (unlikely(ret == -EAGAIN))
 		goto again;
-	}
 
 	return ret;
 }
-EXPORT_SYMBOL(ida_alloc_range);
+EXPORT_SYMBOL(ida_simple_get);
 
 /**
  * ida_simple_remove - remove an allocated id.
  * @ida: the (initialized) ida.
  * @id: the id returned by ida_simple_get.
  */
-void ida_free(struct ida *ida, unsigned int id)
+void ida_simple_remove(struct ida *ida, unsigned int id)
 {
 	unsigned long flags;
 
 	BUG_ON((int)id < 0);
-	xa_lock_irqsave(&ida->ida_rt, flags);
+	spin_lock_irqsave(&simple_ida_lock, flags);
 	ida_remove(ida, id);
-	xa_unlock_irqrestore(&ida->ida_rt, flags);
+	spin_unlock_irqrestore(&simple_ida_lock, flags);
 }
-EXPORT_SYMBOL(ida_free);
+EXPORT_SYMBOL(ida_simple_remove);
 
 /**
  * ida_init - initialize ida handle
