7b7c5f976ac87aecfdfc3a9c0e71203de5602b4a
root <root@dstbx.speedport.ip>
2026-02-01 17:09:49 +0100

    lets hope this undid old susfs patches

--- fs/dcache.c
+++ fs/dcache.c
@@ -2236,12 +2236,6 @@ seqretry:
 			if (dentry->d_name.hash != hashlen_hash(hashlen))
 				continue;
 
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-			if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
-				continue;
-			}
-#endif
-
 			*seqp = seq;
 			switch (slow_dentry_cmp(parent, dentry, seq, name)) {
 			case D_COMP_OK:
@@ -2339,12 +2333,6 @@ struct dentry *__d_lookup(const struct dentry *parent, const struct qstr *name)
 
 		if (dentry->d_name.hash != hash)
 			continue;
-
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-		if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
-			continue;
-		}
-#endif
 
 		spin_lock(&dentry->d_lock);
 		if (dentry->d_parent != parent)
