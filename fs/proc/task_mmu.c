b9b8a0c0ab4c99cccda8251418b1f10452b6d22b
azazel@dstbx <68147363+azazelcodes@users.noreply.github.com>
2026-03-06 17:10:52 +0100

    move bandaidfix gfp_kernel_account also needed in namespace

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -877,7 +877,6 @@ static int pid_smaps_open(struct inode *inode, struct file *file)
 	return do_maps_open(inode, file, &proc_pid_smaps_op);
 }
 
-#define GFP_KERNEL_ACCOUNT (GFP_KERNEL | __GFP_NOACCOUNT)
 static int smaps_rollup_open(struct inode *inode, struct file *file)
 {
 	int ret;
